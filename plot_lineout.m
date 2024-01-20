eb241c19bdc4cf843d2dbb96f527e7dcb7dfb248
Ryan Duncan <raduncan313@gmail.com>
2024-01-19 21:55:27 -0800

    fixed plot_rois

---
+++ plot_lineout.m
@@ -0,0 +1,1 @@
+function d = plot_lineout(d)
--- plot_rois.m
+++ plot_rois.m
@@ -35,3 +35,37 @@ function [f, d] = plot_rois(d, n)
     xlabel('Time (ps)')
     ylabel('Intensity (norm.)')
 end
+
+function [d, f] = roi_mask(d, varargin)
+
+    if ~isfield(d, 'masks')
+        d.masks = {};
+    end
+    
+    imsum = sum(d.off.imgs, 3);
+    if nargin == 1
+        f = figure;
+        im = imagesc(imsum);
+        hold on
+        set(gca, 'colorscale', 'log');
+        title('Select ROI')
+    else
+        f = varargin{1};
+        figure(f);
+        hold on
+    end
+    
+    [mask2D, x, y] = roipoly;
+    plot(x, y, 'linewidth', 2);
+
+    mask3D = repmat(mask2D, [1,1,length(d.off.scan_var)]);
+    imgs_mask_on = d.on.imgs.*mask3D;
+    imgs_mask_off = d.off.imgs.*mask3D;
+    sig = squeeze(sum(imgs_mask_on, [1,2]))./mean(squeeze(sum(imgs_mask_off, [1,2])));
+    
+    mask.mask = mask2D;
+    mask.type = 'ROI';
+    mask.roi = [x y];
+    mask.sig = sig;
+    d.masks{end+1} = mask;
+end
