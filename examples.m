c5a5de1c52e5c8a4e268b93869bfc7a080be9f76
Ryan Duncan <raduncan313@gmail.com>
2024-03-10 21:22:24 -0700

    adding examples

--- examples.m
+++ examples.m
@@ -15,19 +15,3 @@ d.auto_signal(2, 3, 5, 0.7)
 
 % f = d.plotdatacube('on', [0 100], []);
 % f = d.plot_lineout();
-
-%% Helper functions for preprocessing
-
-function d = load_and_preprocess(cubedir, runs, t0, info)
-    d = read_cubes(cubedir, runs);
-    d = norm_i0(d);
-    d = orient_SACLA(d);
-    d.on.scan_var = d.on.scan_var - t0;
-    d.off.scan_var = d.off.scan_var - t0;
-    d.info = info;
-end
-
-function d = orient_SACLA(d)
-    d.on.imgs = permute(d.on.imgs, [2,1,3]);
-    d.off.imgs = permute(d.off.imgs, [2,1,3]);
-end
